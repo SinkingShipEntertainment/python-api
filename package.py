a61395336feffac0f6d0cea37e3392a76c329b27
Marcelo Sercheli <msercheli@gmail.com>
2024-03-11 15:30:37 -0400

    New version

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "sg_api"
 
-version = "3.3.2"
+version = "3.4.2"
 
 authors = [
     "Shotgrid"
@@ -12,19 +12,8 @@ description = \
     """
 
 with scope("config") as c:
-    # Determine location to release: internal (int) vs external (ext)
-
-    # NOTE: Modify this variable to reflect the current package situation
-    release_as = "ext"
-
-    # The `c` variable here is actually rezconfig.py
-    # `release_packages_path` is a variable defined inside rezconfig.py
-
     import os
-    if release_as == "int":
-        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_INT"]
-    elif release_as == "ext":
-        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
+    c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
 
 requires = [
 ]
@@ -33,7 +22,6 @@ private_build_requires = [
 ]
 
 variants = [
-    ["platform-linux", "arch-x86_64", "os-centos-7"],
 ]
 
 build_command = "rez python {root}/rez_build.py"
